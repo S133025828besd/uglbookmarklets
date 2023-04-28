4146e3b51933455ed64fcdaa6023dd1778bf9a19
EGLR <117935328+S133025828besd@users.noreply.github.com>
2023-04-28 10:55:30 -0600

    Create ugl.js

---
+++ ugl.js
@@ -0,0 +1,10 @@
+let a = prompt("Which Game? (Type the number)\n1 = Pixel Speedrun\n2 = Throw A Potato\n3 = Slender Multiplayer\n4 = Wheelie Bike");
+if (a==1) {
+  {((function()%7Bvar a,b,c;c="https://games.construct.net/2363/latest",b=document.createElement("iframe"),b.setAttribute("src",c),b.setAttribute("id","rusic-modal"),b.setAttribute("style","position: fixed; z-index: 999999; width: 100%; height: 100%; right: 0px; top: 0px; border: 0px solid #8834af; overflow: hidden; background-color: #fff;"),a=document.getElementsByTagName("body")%5B0%5D,a.appendChild(b)%7D)).call(this)}
+} else if (a==2) {
+   {((function()%7Bvar a,b,c;c="https://games.construct.net/27703/latest",b=document.createElement("iframe"),b.setAttribute("src",c),b.setAttribute("id","rusic-modal"),b.setAttribute("style","position: fixed; z-index: 999999; width: 100%; height: 100%; right: 0px; top: 0px; border: 0px solid #8834af; overflow: hidden; background-color: #fff;"),a=document.getElementsByTagName("body")%5B0%5D,a.appendChild(b)%7D)).call(this)}
+} else if (a==3) {
+   {((function()%7Bvar a,b,c;c="https://games.construct.net/6059/latest",b=document.createElement("iframe"),b.setAttribute("src",c),b.setAttribute("id","rusic-modal"),b.setAttribute("style","position: fixed; z-index: 999999; width: 100%; height: 100%; right: 0px; top: 0px; border: 0px solid #8834af; overflow: hidden; background-color: #fff;"),a=document.getElementsByTagName("body")%5B0%5D,a.appendChild(b)%7D)).call(this)}
+} else if (a==4) {
+   {((function()%7Bvar a,b,c;c="https://games.construct.net/4061/latest",b=document.createElement("iframe"),b.setAttribute("src",c),b.setAttribute("id","rusic-modal"),b.setAttribute("style","position: fixed; z-index: 999999; width: 100%; height: 100%; right: 0px; top: 0px; border: 0px solid #8834af; overflow: hidden; background-color: #fff;"),a=document.getElementsByTagName("body")%5B0%5D,a.appendChild(b)%7D)).call(this)}
+}
